b90061a5fae17314e3f235059e983ace6c7f56ae
Abel Bodogan <bodoganabel@gmail.com>
2024-05-19 12:24:26 +0200

    Working react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
+import React, { Children, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type IFile = {
+  name: string,
+  children?: IFile[]
+}
+
+const files: IFile[] = [
+  {
+    name: "root",
+    children: [{
+      name: "node_modules",
+      children: [{
+        name: "joi",
+        children: [{
+          name: "node_modules",
+          children: [{
+            name: "joi",
+          },
+          { name: "Hello" }
+          ]
+        },
+        ]
+      },
+      {
+        name: "package.json"
+      },
+      {
+        name: "vite.config.ts"
+      }
+      ]
+    }
+    ]
+  }]
+
+
+function Entry({ file, depth }: { file: IFile, depth: number }) {
+
+  const [isExpanded, setIsExpanded] = useState(false)
+
+  return <div className='' style={{ textAlign: "left", cursor: 'pointer' }}>
+    <div onClick={() => { setIsExpanded(!isExpanded) }}>
+      {file.children && (isExpanded ? "-":"+")}{file.name}
+    </div>
+    {isExpanded && <div style={{ paddingLeft: `1rem` }}>
+        {file.children !== undefined && file.children.map((child) => (
+          <div style={{}}>
+            <Entry file={child} depth={depth + 1} />
+          </div>
+        ))}
+      </div>}
+  </div>
+
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ display: "flex", justifyContent: "start" }}>
+      {files.map(file => <Entry file={file} depth={0}></Entry>)}
     </div>
   );
 }
